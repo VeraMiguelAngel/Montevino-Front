a0e189c6f4a0aa9672403d7d6618d0eb2e1e00d9
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2026-05-27 08:07:14 -0300

    modified:   src/app/host/ordenes/cerradas/page.tsx

--- front/src/app/host/ordenes/cerradas/page.tsx
+++ front/src/app/host/ordenes/cerradas/page.tsx
@@ -115,7 +115,7 @@ export default function HostOrdenesCorradasPage() {
                       <p className="text-sm text-gray-500">
                         Mesa:{" "}
                         <span className="font-medium text-gray-700">
-                          #{reservation.table.number || reservation.table.id}
+                          #{reservation.table.tableNumber || reservation.table.id}
                         </span>
                       </p>
                     )}
